d17fea341a0e9748e7819a4ef4ca2007eda31c94
Leeky0615 <rbdus7174@naver.com>
2022-01-16 15:25:45 +0900

    feat : development search api controller

---
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -0,0 +1,28 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.querydsl.dto.MemberSearchCond;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCond condition) {
+        log.info("condition={}", condition.getUsername());
+        log.info("condition={}", condition.getTeamName());
+        log.info("condition={}", condition.getAgeGoe());
+        log.info("condition={}", condition.getAgeLoe());
+        return memberJpaRepository.search(condition);
+    }
+}
